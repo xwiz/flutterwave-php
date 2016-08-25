53b1e7cc33e964d9439a647874cb8be272f31593
Ridwan Olalere <ridwan@Ridwans-MacBook-Pro.local>
2016-08-25 11:29:33 +0100

    added decrypt

--- src/FlutterEncrypt.php
+++ src/FlutterEncrypt.php
@@ -27,4 +27,23 @@ class FlutterEncrypt
 
         return base64_encode($encData);
     }
+
+    public static function decrypt3Des($data, $secret)
+   {
+       //Generate a key from a hash
+       $key = md5(utf8_encode($secret), true);
+
+       //Take first 8 bytes of $key and append them to the end of $key.
+       $key .= substr($key, 0, 8);
+
+       $data = base64_decode($data);
+
+       $data = mcrypt_decrypt('tripledes', $key, $data, 'ecb');
+
+       $block = mcrypt_get_block_size('tripledes', 'ecb');
+       $len = strlen($data);
+       $pad = ord($data[$len-1]);
+
+       return substr($data, 0, strlen($data) - $pad);
+   }
 }
--- tests/FlutterEncryptTest.php
+++ tests/FlutterEncryptTest.php
@@ -4,8 +4,17 @@ use Flutterwave\FlutterEncrypt;
 class FlutterEncryptTest extends \PHPUnit_Framework_TestCase {
 
   public function testEncryptWorks() {
-    $b = new FlutterEncrypt();
-    $result = $b->encrypt3Des("Ridwan", "tk_NabYp2XjZ6G9WwdFruzK");
+    $result = FlutterEncrypt::encrypt3Des("Ridwan", "tk_NabYp2XjZ6G9WwdFruzK");
     $this->assertNotEmpty($result);
+    return $result;
+  }
+
+  /**
+   * @depends testEncryptWorks
+   */
+  public function testDecryptWorks($result) {
+    $rawData = "Ridwan";
+    $res = FlutterEncrypt::decrypt3Des($result, "tk_NabYp2XjZ6G9WwdFruzK");
+    $this->assertEquals($rawData, $res);
   }
 }
